2a31c3f2f23a5b1dc543fc96f6b19e80652baaa2
Mateusz <99211845+MateuszGadzala@users.noreply.github.com>
2024-08-10 18:24:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operators
+{
+    class OperatorsProgram
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Hi! In this leesin we talk about operators!");
+        }
+    }
+}
